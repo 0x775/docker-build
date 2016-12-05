dbfa80b6c493caa352410aa0a98cbf2dd26a9e67
0x775 <inorz@qq.com>
2016-12-05 19:24:59 +0800

    Update php-fpm.sh

--- scripts/php-fpm.sh
+++ scripts/php-fpm.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#docker run -d --name php-fpm --link redis:redis -p 9000:9000 -v /Users/ph4nf4n/docker/api.xnongren.com/www:/www php-fpm
-docker run -d --name php-fpm --link redis:redis -P -v /Users/ph4nf4n/docker/www:/www php-fpm
+#docker run -d --name php-fpm --link redis:redis -p 9000:9000 -v /Users/apple/docker/api.xnong.com/www:/www php-fpm
+docker run -d --name php-fpm --link redis:redis -P -v /Users/apple/docker/www:/www php-fpm
