259d71040c9362a889864c93273d36e6cf3b314b
0x775 <inorz@qq.com>
2016-12-05 19:21:57 +0800

    Delete nginx.sh

--- scrpits/nginx.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker run -d --name api-xnongren -p 80:80 --link php-fpm:php-fpm -v /vagrant/api.xnongren.com/www:/www -v /vagrant/api.xnongren.com/nginx.conf:/etc/nginx/nginx.conf nginx
