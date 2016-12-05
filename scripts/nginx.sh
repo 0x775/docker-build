fe532a6a42b2b37543a20f7873e8228d07a8a48a
0x775 <inorz@qq.com>
2016-12-05 19:24:34 +0800

    Update nginx.sh

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#docker run -d --name api-xnongren -p 80:80 --link php-fpm:php-fpm -v /Users/ph4nf4n/docker/api.xnongren.com/www:/www -v /Users/ph4nf4n/docker/api.xnongren.com/nginx.conf:/etc/nginx/nginx.conf nginx
-docker run -d --name web -p 80:80 --link php-fpm:php-fpm -v /Users/ph4nf4n/docker/www:/www -v /Users/ph4nf4n/docker/site-enabled/nginx:/etc/nginx/nginx.conf -v /Users/ph4nf4n/docker/site-enabled:/etc/nginx/site-enabled nginx
+#docker run -d --name api-xnongren -p 80:80 --link php-fpm:php-fpm -v /Users/apple/docker/api.xnong.com/www:/www -v /Users/apple/docker/api.xnong.com/nginx.conf:/etc/nginx/nginx.conf nginx
+docker run -d --name web -p 80:80 --link php-fpm:php-fpm -v /Users/apple/docker/www:/www -v /Users/apple/docker/site-enabled/nginx:/etc/nginx/nginx.conf -v /Users/apple/docker/site-enabled:/etc/nginx/site-enabled nginx
